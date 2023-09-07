cdd77c200c5cc08a139fcf71aa95173bdcd915f2
Charrière Maxime <51918753+maximecharriere@users.noreply.github.com>
2023-09-07 13:17:57 +0200

    include PS script in bat file

--- SamsungNotesInstaller.bat
+++ SamsungNotesInstaller.bat
@@ -1,12 +1,24 @@
-@echo off
+@ECHO OFF
 
-SET AdminRequired=true
+SET AdminRequired=False
+SET UseExternalScript=False
+
 SET PowerShellScriptPath=%~dpn0.ps1
+SET PowerShellCommand=winget install 'Samsung Notes' --source msstore --accept-package-agreements --accept-source-agreements
 
-if "%AdminRequired%"=="true" (
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
+if "%AdminRequired%"=="True" (
+    if "%UseExternalScript%"=="True" (
+        PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
+    ) else (
+        PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""%PowerShellCommand%""' -Verb RunAs}";
+    )
 ) else (
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
+    if "%UseExternalScript%"=="True" (
+        PowerShell -NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""
+    ) else (
+        PowerShell -NoProfile -ExecutionPolicy Bypass -Command ""%PowerShellCommand%""
+    )
+    
 )
 
 pause
